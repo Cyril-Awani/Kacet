57d6cf33160f70213a42478d0aa05596b74843bc
Cyril-Awani <cyrilawani27@gmail.com>
2024-11-17 12:40:08 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} antialiased`}
+        className={`${geistMono.variable} antialiased`}
       >
         {children}
       </body>
